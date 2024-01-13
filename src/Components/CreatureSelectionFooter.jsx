05975159b7d4e0ef886e65672ffec874826c5397
Ali Elbasuony <alielbasuony8@gmail.com>
2024-01-14 01:45:49 +0200

    final commit

--- src/Components/CreatureSelectionFooter.jsx
+++ src/Components/CreatureSelectionFooter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ProceedButton from './ProceedButton'
 function CreatureSelectionFooter() {
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <div className='flex flex-col justify-center items-center mt-4 sm:mt-8'>
       <ProceedButton/>
     </div>
   )
